21e4079691b5763e54ebcfdf3f3940b12c9eb190
Dogukan HAN <mail@dogukanhan.com>
2019-03-03 13:41:06 +0300

    This commit contains stardart spring boot project structure. Web,Security,JPA,Devtools,H2,Thmeleaf are installed.

---
+++ src/main/java/com/dogukanhan/kodgemisi/simplehr/SimplehrApplication.java
@@ -0,0 +1,16 @@
+package com.dogukanhan.kodgemisi.simplehr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+
+public class SimplehrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplehrApplication.class, args);
+	}
+
+}
