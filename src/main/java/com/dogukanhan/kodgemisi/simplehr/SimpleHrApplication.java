c6c1acf0ea13ae0e3b037b21e2aeee8ef9149256
Dogukan HAN <mail@dogukanhan.com>
2019-03-03 23:17:39 +0300

    WebConfig file added

--- src/main/java/com/dogukanhan/kodgemisi/simplehr/SimpleHrApplication.java
+++ src/main/java/com/dogukanhan/kodgemisi/simplehr/SimpleHrApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class SimpleHrApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/dogukanhan/kodgemisi/simplehr/WebConfig.java
@@ -0,0 +1,12 @@
+package com.dogukanhan.kodgemisi.simplehr;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+}
